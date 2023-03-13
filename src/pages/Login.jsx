4ece9aa5fe430c3de8f954ef0572f0fa6d11aaf1
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-03-13 15:49:13 -0300

    Requisito 2 iniciado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  state = {
+    disableButton: true,
+    name: '',
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      name: value,
+    }, this.verifyCharacters);
+  };
+
+  verifyCharacters = () => {
+    const { name } = this.state;
+    const MIN__LENGTH = 3;
+    if (name.length < MIN__LENGTH) {
+      this.setState({
+        disableButton: true,
+      });
+    } else {
+      this.setState({
+        disableButton: false,
+      });
+    }
+  };
+
   render() {
+    const { disableButton, name } = this.state;
     return (
       <div data-testid="page-login">
-        oi
+        <form onSubmit={ this.handleSubmit }>
+          <input
+            type="text"
+            placeholder="Digite seu nome"
+            data-testid="login-name-input"
+            onChange={ this.handleChange }
+            value={ name }
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ disableButton }
+            onClick={ createUser(name) }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
