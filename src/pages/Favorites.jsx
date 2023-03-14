63bf455085cb1057cb19aea06f623d55ee4d5999
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-03-14 14:51:52 -0300

    Requisito 12 iniciado

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  state = {
+    savedSongs: [],
+    isLoadingPage: true,
+    // isLoading: false,
+    isFavorite: true,
+  };
+
+  componentDidMount() {
+    this.getSongs();
+    this.setState({
+      isLoadingPage: false,
+    });
+  }
+
+  componentDidUpdate() {
+    this.getSongs();
+  }
+
+  getSongs = async () => {
+    const savedSongs = await getFavoriteSongs();
+    this.setState({
+      savedSongs,
+    });
+  };
+
   render() {
+    const { savedSongs, isFavorite, isLoadingPage } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
+        {
+          isLoadingPage
+            ? <Loading />
+            : (
+              savedSongs.map((music) => (
+                <MusicCard
+                  key={ music.trackId }
+                  trackName={ music.trackName }
+                  previewUrl={ music.previewUrl }
+                  trackId={ music.trackId }
+                  music={ music }
+                  isFavorite={ isFavorite }
+                />
+              ))
+            )
+        }
       </div>
     );
   }
