5d68df3b4ccdc1af1fcb83b4c035d2c8bbd25df4
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-03-13 22:31:35 -0300

    Requisito 7 feito

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
+  state = {
+    newAlbum: [],
+    artistName: '',
+    collectionName: '',
+  };
+
+  componentDidMount() {
+    this.fetchMusic();
+  }
+
+  fetchMusic = async () => {
+    const { match: { params: { id } } } = this.props;
+    const albumMusics = await getMusics(id);
+    const newAlbum = albumMusics.filter((_album, index) => index !== 0);
+    const { artistName } = albumMusics[0];
+    const { collectionName } = albumMusics[0];
+    this.setState({
+      newAlbum,
+      artistName,
+      collectionName,
+    });
+  };
+
   render() {
+    const { newAlbum, artistName, collectionName } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+
+        <h2 data-testid="artist-name">{ artistName }</h2>
+        <h2 data-testid="album-name">{ collectionName }</h2>
+
+        {
+          newAlbum.map((music) => (
+            <MusicCard
+              key={ music.trackId }
+              trackName={ music.trackName }
+              previewUrl={ music.previewUrl }
+            />
+          ))
+        }
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
