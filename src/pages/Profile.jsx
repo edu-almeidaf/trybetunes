2d93120cd0346d0502e6c16f3bd195b68707c052
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-03-15 10:35:12 -0300

    Requisito 13 iniciado

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    isLoading: true,
+    userProfile: {},
+  };
+
+  componentDidMount() {
+    this.getSavedUser();
+  }
+
+  getSavedUser = async () => {
+    const userProfile = await getUser();
+    this.setState({
+      userProfile,
+      isLoading: false,
+    });
+  };
+
   render() {
+    const { isLoading, userProfile } = this.state;
+    const { name, email, description, image } = userProfile;
     return (
       <div data-testid="page-profile">
         <Header />
+        {
+          isLoading
+            ? <Loading />
+            : (
+              <div>
+                <h2>{ name }</h2>
+                <p>{ email }</p>
+                <p>{ description }</p>
+                <img
+                  src={ image }
+                  alt={ name }
+                  data-testid="profile-image"
+                />
+                <Link to="/profile/edit">
+                  Editar perfil
+                </Link>
+              </div>
+            )
+        }
       </div>
     );
   }
